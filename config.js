a0408f8f73f2be9815890b2bb3a729803f9da99b
1MishraBhavesh <74255631+1MishraBhavesh@users.noreply.github.com>
2021-07-02 20:47:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,19 +17,4 @@ var firebaseConfig = {
 
 
 
-  import firebase from 'firebase'
-require('@firebase/firestore')
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDpRxX8pLs-KGRAlt0NH_bfxeFwSDbdaxM",
-    authDomain: "wily-app-90ed6.firebaseapp.com",
-    projectId: "wily-app-90ed6",
-    storageBucket: "wily-app-90ed6.appspot.com",
-    messagingSenderId: "372763404902",
-    appId: "1:372763404902:web:bc87ed0a06e04d86925ba7"
-  };
- 
-  if (!firebase.apps.length) {
-firebase.initializeApp(firebaseConfig);
-}
-export default firebase.firestore;
+  
